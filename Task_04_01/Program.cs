9ae77d7933e8b29a0c1c8451ca3479d3ace89616
Egurnova Elena <gamedev409@yandex.ru>
2025-01-11 18:59:47 +0500

    add projects for example

---
+++ Task_04_01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task_04_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
