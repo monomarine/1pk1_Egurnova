8d79c8e20ab2b5dbe0d06cb029bdf06611b69f55
Елена <test@email.ru>
2025-01-18 10:24:37 +0500

    PZ_03_02 done

--- Task_03_02/Program.cs
+++ Task_03_02/Program.cs
@@ -2,9 +2,34 @@
 {
     internal class Program
     {
+        /*игра "угадай-ка" программа загадывает число, 
+         * пользователь за конечное число шагов должен 
+         * это число угадать в случае неправильного ответа 
+         * программа подсказывает, больше ли введенное 
+         * число относительно загаданного или меньше
+*/
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random rnd = new Random(); //объект рандома
+            int countOfAttempt = 5; //количество попыток
+            int x = rnd.Next(100); //компьютер загадывает число
+
+            Console.WriteLine("отгадайте числов в диапазоне от 0 до 100 за 5 попыток");
+            for (int i = 0; i < countOfAttempt; i++) //цикл ограничен количеством попыток
+            {
+                int userAttempt = int.Parse(Console.ReadLine()); //считывает ответ пользователя
+                if (userAttempt == x) //если пользователь ввел загаданное число
+                {
+                    Console.WriteLine("Угадали!");
+                    break;
+                }
+                else if (userAttempt > x) //подсказка
+                    Console.WriteLine("неправильно, загаданное число меньше");
+                else Console.WriteLine("неправильно, загаданное число больше"); //подстказка
+            }
+
+            Console.WriteLine("загаданное число - " + x);
+
         }
     }
 }
