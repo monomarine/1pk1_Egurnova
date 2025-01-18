196d92b6aa8eb354246ab465900cfa7096f102c8
Елена <test@email.ru>
2025-01-18 09:54:02 +0500

    Task_03_03 done

--- Task_03_01/Program.cs
+++ Task_03_01/Program.cs
@@ -2,9 +2,21 @@
 {
     internal class Program
     {
+        /*Выведите классическую таблицу умножения 
+         * используя вложенные циклы
+         */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            for( int i = 2; i < 11;  i++ ) //внешний цикл
+            {
+                Console.WriteLine($"умножение на {i}");
+
+                for (int j = 2; j< 11; j++) //внутренний цикл
+                {
+                    Console.WriteLine($"{i} * {j} = {i*j}"); //интеполированный вывод
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ Task_03_03/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Task_03_03
+{
+    internal class Program
+    {
+        /*Дано целое число N (> 0), являющееся некоторой степенью числа 2: 
+         * N = 2^K. Найти целое число K — показатель этой степени.
+            Пример
+            Входные данные
+            1024
+            Выходные
+            10
+         */
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                Console.WriteLine("введите число N - для проверки на степень числа 2");
+                int number = int.Parse(Console.ReadLine());
+
+                int power = 1; //начальное значение степени
+                int osn = 2; //основание которое будем в цикле умножать на само себя неопределенное количество раз
+                while (osn != number && osn < number)
+                {
+                    osn *= 2;
+                    power++;
+                }
+
+                //проверка на корректность полученного значения
+                var temp = Math.Pow(2, power);
+                if (temp == number)
+                    Console.WriteLine($"число {number} является степенью {power} числа 2");
+                else
+                    Console.WriteLine("число не является степенью числа 2");
+
+            }
+        }
+    }
+}
