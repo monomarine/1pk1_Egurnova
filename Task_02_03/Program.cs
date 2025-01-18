7de6431e09ba5e4e5783d08c8953ef9b090d6d16
Egurnova Elena <gamedev409@yandex.ru>
2025-01-18 07:52:20 +0500

    pz_02_03 done

--- Task_02_03/Program.cs
+++ Task_02_03/Program.cs
@@ -9,80 +9,83 @@
             Сменить пароль
             Удалить учетку
         В зависимости от выбранного режима произвести соответствующие действия с пользовательскими данными и выведите соответствующие сообщения.
+        */
 
-         */
         static void Main(string[] args)
         {
-            //информационное сообщение пользователю. (старайтей дать инструкцию
-            //пользователю для работы
-            Console.WriteLine("выберите желаемое действие\n" +
-                "\t 1 - зарегестрироваться\n" +
-                "\t 2 - сменить логин\n" +
-                "\t 3 - сменить пароль\n" +
-                "\t 4 - удалить учетку.");
-
-            //переменная для считывания выбранного режима работы
-            string? mode = Console.ReadLine();
-
             //переменные для хранения логина и пароля
             string login = "";
             string password = "";
 
-
-            switch(mode)
+            while (true)
             {
-                case "1": //ветка регистрации пользователя
+                Console.Clear(); //очистка консоли
 
-                    Console.WriteLine("введите логин нового пользователя");
-                    string? tempLogin = Console.ReadLine();  //временная переменная для нового логина
-                    //проверка - если пользователь ввел не тот же самый логин и не пустой
-                    if (tempLogin != login && tempLogin != null)
+                //информационное сообщение пользователю. (старайтей дать инструкцию
+                //пользователю для работы
+                Console.WriteLine("выберите желаемое действие\n" +
+                    "\t 1 - зарегестрироваться\n" +
+                    "\t 2 - сменить логин\n" +
+                    "\t 3 - сменить пароль\n" +
+                    "\t 4 - удалить учетку.\n" +
+                    "\t 5 - проверить данные");
+
+                //переменная для считывания выбранного режима работы
+                string? mode = Console.ReadLine();
+
+                switch (mode)
+                {
+                    case "1": //ветка регистрации пользователя
+
+                        Console.WriteLine("введите логин нового пользователя");
+                        string? tempLogin = Console.ReadLine();  //временная переменная для нового логина
+                                                                 //проверка - если пользователь ввел не тот же самый логин и не пустой
                         login = tempLogin; //перезапись логина
-                    else
-                    {
-                        Console.WriteLine("логин совпадает с ранее введенным\n" +
-                            "прерывание регистрации");
-                        break;
-                    }
-
-                    Console.WriteLine("введите пароль нового пользователя");
-                    string? tempPass = Console.ReadLine();  //временная переменная для нового логина
-                    //проверка - если пользователь ввел не тот же самый пароль и не пустой
-                    if (tempPass != password && tempPass != null)
+                        
+                        Console.WriteLine("введите пароль нового пользователя");
+                        string? tempPass = Console.ReadLine();  //временная переменная для нового логина
+                                                                //проверка - если пользователь ввел не тот же самый пароль и не пустой
                         password = tempPass; //перезапись пароля
-                    else
-                    {
-                        Console.WriteLine("пароль совпадает с ранее введенным\n" +
-                            "прерывание регистрации");
                         break;
-                    }
 
-                    break;
+                    case "2": // смена логина
+                        if (login != null)
+                            Console.WriteLine("ваш текущий логин - " + login +
+                                "\nвведите новый логин");
+                        tempLogin = Console.ReadLine();
+                        if (tempLogin != login && tempLogin != null)
+                            login = tempLogin;
+                        else
+                            Console.WriteLine("введен некорректный логин");
+                        break;
 
-                case "2": // смена логина
-                    if (login != null)
-                        Console.WriteLine("ваш текущий логин - " + login +
-                            "\nвведите новый логин");
-                     tempLogin = Console.ReadLine();
-                    if (tempLogin != login && tempLogin != null)
-                        login = tempLogin;
-                    else
-                        Console.WriteLine("введен некорректный логин");
-                    break;
+                    case "3": // смена пароля
+                        if (password != null)
+                            Console.WriteLine("ваш текущий пароль - " + password +
+                                "\nвведите новый пароль");
+                        tempPass = Console.ReadLine();
+                        if (tempPass != login && tempPass != null)
+                            password = tempPass;
+                        else
+                            Console.WriteLine("введен некорректный пароль");
+                        break;
 
-                case "3": // смена пароля
-                    Console.WriteLine();
-                    break;
+                    case "4": // удаление учетки
+                        login = "";
+                        password = "";
+                        Console.WriteLine("данные удалены");
+                        break;
 
-                case "4": // удаление учетки
-                    login = "";
-                    password = "";
-                    Console.WriteLine("данные удалены");
-                    break;
-                
-                default:
-                    Console.WriteLine("вы выбрали некорректный режим");
-                    break ;
+                    case "5": //проверка данных
+                        Console.WriteLine("ваш логин: " + login +
+                            "\nваш пароль: " +password);
+                        break;
+
+                    default:
+                        Console.WriteLine("вы выбрали некорректный режим");
+                        break;
+                }
+                Console.ReadKey();
             }
 
         }
