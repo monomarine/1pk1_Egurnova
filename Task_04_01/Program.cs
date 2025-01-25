d691c49916960eba974a589948bdd96b1d6851a3
Egurnova Elena <gamedev409@yandex.ru>
2025-01-25 21:13:42 +0500

    добавлено выделение цветом в 04.01

--- Task_04_01/Program.cs
+++ Task_04_01/Program.cs
@@ -6,6 +6,8 @@
          * в диапазоне от 10 до 99 вкл. 
          * Вывести количество пар соседних 
          * элементов массива, кратных пяти.
+         * 
+         * - дополнительно вывести массив повторно отметив цветов эти числа
         */
         static void Main(string[] args)
         {
@@ -19,17 +21,33 @@
                 Console.Write(numbers[index] + " ");
             }
 
+            bool[] isPair = new bool[30]; //массив флагов для обозначения найденных пар
+
             //поиск пар начиная со 2 элемента
             int countOfPairs = 0;
             for (int i = 1; i< numbers.Length; i++)
             {
                 if (numbers[i] % 5 == 0 && numbers[i-1] % 5 == 0)
                 { 
-                    //TODO - доделать вывод цветом
                     countOfPairs++;
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.WriteLine(numbers[i]);
-                    Console.BackgroundColor = ConsoleColor.Black;
+                    isPair[i] = true;
+                    isPair[i-1] = true;
+              
+                }
+            }
+
+            //вывод с выделением цветом
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (isPair[i])//если есть отметка в массиве isPair что этот элемент является парным, то выделяем цветом
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(numbers[i]+ " ");
+                    Console.ForegroundColor= ConsoleColor.White;
+                }
+                else //в противном случае при выводе фон не меняется
+                {
+                    Console.Write(numbers[i]+ " ");
                 }
             }
 
