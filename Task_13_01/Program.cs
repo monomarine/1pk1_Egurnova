47d171326e3abcf3b25c8bb938336dd4e6eb1b3c
Egurnova Elena <gamedev409@yandex.ru>
2025-03-01 08:27:57 +0500

    13_01 student class done

--- Task_13_01/Program.cs
+++ Task_13_01/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Student student = new Student("Иван", "Иванов");
+            //Console.WriteLine(student.GetInfo());
+
+
+            //Student student2 = new Student("Сергей", "Петров", "Иванович", new DateTime(2005, 10, 05));
+            //Console.WriteLine(student2.GetInfo());
+
+            Student st = new Student();
+            st.Name = "Петр";
+            st.Surname = "Иванов";
+            st.Patronomyc = null;
+            st.Birthday = DateTime.Now
+                ;
+
+            Console.WriteLine(st.GetInfo());
         }
     }
 }
