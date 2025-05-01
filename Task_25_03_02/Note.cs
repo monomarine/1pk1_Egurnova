bc6dc2acd2f55c886ea2662ac5ccfdfbd260739a
marine mono <monomarinee@gmail.com>
2025-05-01 10:26:39 +0500

    25_03_02 дополнитиельный пример по смене значения перечисления при загрузке/сохранении  сериализированных данных

---
+++ Task_25_03_02/Note.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public enum Category { Important, NotImportant}
+
+    internal class Note
+    {
+        public string Text { get; set; }
+        public Category Category { get; set; }
+
+        public Note (string text, Category category)
+        {
+            Text = text;
+            Category = category;
+        }
+
+        public override string ToString()
+        {
+            return $"{Text}\n" +
+                $"категория - {Category}";
+        }
+    }
+}
---
+++ Task_25_03_02/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.CodeDom.Compiler;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static string fileName = "Notes.json";
+        static void Main(string[] args)
+        {
+            Generate();
+            var nodes = LoadNotes();
+
+            foreach (var node in nodes)
+            {
+                Console.WriteLine("до смены категории: " + node);
+                node.Category = node.Category == Category.NotImportant ?
+                     Category.Important :
+                     Category.NotImportant;
+
+                Console.WriteLine("после смены категории: " + node);
+            }
+
+            SaveNodes(nodes);
+        }
+
+        private static void Generate()
+        {
+            List<Note> notes = new()
+            {
+                new Note("сделать ДЗ по программированию", Category.Important),
+                new Note("отдыхать!", Category.NotImportant),
+            };
+
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+            };
+           
+            string jsonString = JsonSerializer.Serialize(notes, options);
+            File.WriteAllText(fileName, jsonString);
+        }
+
+        private static List<Note> LoadNotes()
+        {
+           return JsonSerializer.Deserialize<List<Note>>(File.ReadAllText(fileName));  
+        }
+
+        private static void SaveNodes(List<Note> notes)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+            };
+            File.WriteAllText(fileName, JsonSerializer.Serialize(notes, options));
+        }
+    }
+}
